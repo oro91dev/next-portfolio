acaa3ad5a87af5570a14006822ba5a15a50b3d3b
OroDev <oro91dev@gmail.com>
2019-01-25 23:04:28 +0100

    added new functionality

--- pages/hireme.js
+++ pages/hireme.js
@@ -1,9 +1,11 @@
 const HireMe = () => (
     <div>
         <h1>Hire me</h1>
-        <p>You can hire me at <a href="#"></a></p>
+        <p>You can hire me at {" "} 
+            <a href="mailto:expertcoder@gmail.com">orodevtech@gmail.com</a>
+        </p>
 
     </div>
-)
+);
 
 export default HireMe;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
-const Index = () => ( <div>
+import Link from "next/link";
+import Layout from "../components/Layout";
+
+const Index = () => ( 
+
+  <Layout title="home">
     <h1>Home</h1>
-    Welcome to the home page
-    </div>
+    <Link href="/about">
+        <a>Go to about</a>
+    </Link>
+        <p>Welcome to the home page</p>
+    </Layout>
 );
 
 export default Index;
